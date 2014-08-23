247fb4fb5836f5ab04442b0c06ba3bc334dff021
Wilson Lau <wilsonhlau@gmail.com>
2014-08-22 21:44:46 -0700

    Comment added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do:
+## This R script contains 2 functions. makeCacheMatrix() creates an object
+## that contains a matrix and its cached inverse if available. cacheSolve()
+## calculate the inverse of the matrix of a given object created by
+## makeCacheMatrix() if not availabe and then store the value in the object.
 
-## Write a short comment describing this function
+
+## Write a short comment describing this function: 
+## makeCacheMatrix take a matrix object as an argument. It returns a list of 4 
+## elements, of which each is a function. The getMatrix function returns the 
+## matrix object, and the setMatrix function sets/re-sets the matrix object. The
+## getInverse fnction returns the inverse of the matrix object or NULL if it's
+## not available. The setInverse function sets/re-sets the inverse of the matrix
+## object.
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -24,7 +34,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function:
+## cacheSolve takes a special object created by makeCacheMatrix() as the
+## argument. It returns the cached inverse from the special object if it's
+## availabe. Otherwise, it calculates the inverse, store the result in the
+## special object and then return the inverse matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
